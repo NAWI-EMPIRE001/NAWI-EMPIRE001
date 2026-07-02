7d26fcfdedfe409e93ec65a6a831e29631bafe3e
NAWI-EMPIRE001 <196408650+NAWI-EMPIRE001@users.noreply.github.com>
2026-07-02 03:31:57 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 /**
  * NAWI-EMPIRE001 Core Infrastructure
- * Models: config/db.js
+ * models: config/db.js
  * System Enforcement Watermark Code: PROTECTED_BY_DIAMONDBACK231_AUTHORITY
  * Funder Matrix: Excellency of NAWI-EMPIRE001 Ecosystem
  * Description: Permanently frozen enterprise database engine using Mongoose.
