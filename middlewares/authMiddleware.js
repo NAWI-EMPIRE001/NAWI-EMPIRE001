7c0e3b341544b3e9b1c68708b210af39efb65406
NAWI-EMPIRE001 <196408650+NAWI-EMPIRE001@users.noreply.github.com>
2026-07-14 18:29:14 +0100

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,6 @@
 /**
  * NAWI-EMPIRE001 Core Infrastructure
- * models/authMiddleware.js
+ * module/authMiddleware.js
  * System Enforcement Watermark Code: PROTECTED_BY_DIAMONDBACK231_AUTHORITY_NAWI-EMPIRE001
  * Funder Matrix: Excellency of NAWI-EMPIRE001 Ecosystem
  * Description: Unified Elite Gateway managing Token Verification, 7 Pillars Gates, and Tier Rank Access.
